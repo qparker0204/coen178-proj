887a5c93538b1f0e13f960b2d421ea9a4c317054
Quinten Parker <quinten@Quintens-MacBook-Air.local>
2016-11-27 15:08:08 -0800

    update UI and JS

--- js/soprompt.js
+++ js/soprompt.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
   $("#vMachStat").click(function(){
-    $("#vMachStatForm").show();
+    $("#vMachStatForm").slideToggle();
     $("#vBillForm").hide();
   });
 
   $("#vBill").click(function(){
-    $("#vBillForm").show();
+    $("#vBillForm").slideToggle();
     $("#vMachStatForm").hide();
   });
 
   $("#cServCon").click(function(){
-    $("#cServConForm").show();
+    $("#cServConForm").slideToggle();
     $("#cRepJobForm").hide();
   });
 
   $("#cRepJob").click(function(){
-    $("#cRepJobForm").show();
+    $("#cRepJobForm").slideToggle();
     $("#cServConForm").hide();
   });
 
   $("#uMachStat").click(function(){
-    $("#uMachStatForm").show();
+    $("#uMachStatForm").slideToggle();
   });
 });
